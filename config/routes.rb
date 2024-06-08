731589368ed2ee7988871610db75ec760e4bc9b0
you-u-u <a6cd1f.g@gmail.com>
2024-06-08 14:50:02 +0900

    記録投稿機能作成(routing) fixed #65

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
   
   #delete 'logout', to: 'users/sessions#destroy' #ここログアウト
   
-  
   resources :poses, only: [:show]
   get 'pose/random', to: 'poses#random', as: 'random_pose'
 
+  resources :diaries, only: %i[new create index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
