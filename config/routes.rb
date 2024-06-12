16edea6e3f3cc793e389f4342eea796be0f3fc54
you-u-u <a6cd1f.g@gmail.com>
2024-06-12 16:22:57 +0900

    カレンダー作成/view(index) add #72

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,14 @@ Rails.application.routes.draw do
     delete 'logout', to: 'devise/sessions#destroy'
   end
   #ここLINEログイン
-  #get 'users/auth/line', to: redirect('/users/auth/line')
+  get 'users/auth/line', to: redirect('/users/auth/line')
   
   #delete 'logout', to: 'users/sessions#destroy' #ここログアウト
   
   resources :poses, only: [:show]
   get 'pose/random', to: 'poses#random', as: 'random_pose'
 
-  resources :diaries, only: %i[new create index]
+  resources :diaries, only: %i[new create index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
