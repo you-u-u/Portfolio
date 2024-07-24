98dd8fe425c6d63f3527bfb1de230b6e443d32fd
you-u-u <a6cd1f.g@gmail.com>
2024-07-24 22:25:25 +0900

    TOPページ以外のSwiperを読みこむバグ修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,26 +1,5 @@
 // Entry point for the build script in your package.json
-console.log('application.js is loaded'); // 追加するログ
+console.log('application.js is loaded'); 
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.css';
-
-document.addEventListener('turbo:load', () => {
-  const swiperContainer = document.querySelector('.swiper-container');
-  if (swiperContainer) {
-    const swiper = new Swiper(swiperContainer, {
-      slidesPerView: 'auto',
-      spaceBetween: 20,
-      loop: true,
-      autoplay: {
-        delay: 0,
-        disableOnInteraction: false,
-      },
-      speed: 5000,
-      centeredSlides: true,
-    });
-  } else {
-    console.error('Swiper container not found!');
-  }
-});
