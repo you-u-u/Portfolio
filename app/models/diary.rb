7ecd14f1ca26bb8a13ca882c85565f3b2aec1b61
you-u-u <a6cd1f.g@gmail.com>
2024-07-28 11:30:03 +0900

    uuid変更に伴う修正 add #167

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -14,8 +14,7 @@ class Diary < ApplicationRecord
   private
 
   def only_one_diary_per_day
-    return unless Diary.where(user_id: user_id, date: date).where.not(id: id).exists?
-
+    return unless Diary.where(user_id: user_id, date: date).where.not(uuid: uuid).exists?
     errors.add(:base, '今日のDiaryは登録してあります！また明日も頑張りましょう！')
   end
 end
