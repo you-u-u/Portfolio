d5b38be18b04ca6a998c561939f54ce6e10c7b5e
you-u-u <a6cd1f.g@gmail.com>
2024-08-01 20:48:35 +0900

    lint check修正 add #181

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,5 +1,5 @@
 class DiariesController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_pose, only: %i[new]
   before_action :check_register_diary, only: %i[new create]
 
