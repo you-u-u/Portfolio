8f14ab6845b96825b00c424655db247ea3ed0ead
you-u-u <a6cd1f.g@gmail.com>
2024-08-03 07:39:27 +0900

    CSRF対応

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  OmniAuth.config.allowed_request_methods = [:post, :get]
+  provider :line, ENV.fetch('LINE_KEY', nil), ENV.fetch('LINE_SECRET', nil), {
+    scope: 'profile openid',
+    prompt: 'consent',
+    bot_prompt: 'normal',
+    redirect_uri: 'https://www.yoga-diary-app.com/users/auth/line/callback'
+  }
 end
+
+OmniAuth.config.allowed_request_methods = [:post]
