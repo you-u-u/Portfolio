346a4071ffa3e602c6e9aa39feeaeaec57c4e31f
you-u-u <a6cd1f.g@gmail.com>
2024-08-03 17:15:19 +0900

    修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
-config.omniauth :line, ENV.fetch('LINE_KEY'), ENV.fetch('LINE_SECRET'), {
-  callback_path: "/users/auth/line/callback",
-  request_path: "/users/auth/line",
-  scope: "profile openid",
-  response_type: "code",
-  state: SecureRandom.hex(24)
-}
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :line, ENV.fetch('LINE_KEY', nil), ENV.fetch('LINE_SECRET', nil), {
+    callback_path: "/users/auth/line/callback",
+    request_path: "/users/auth/line",
+    scope: "profile openid",
+    response_type: "code",
+    state: SecureRandom.hex(24)  }
+end
 
 OmniAuth.config.logger = Rails.logger
 OmniAuth.config.allowed_request_methods = [:post]
